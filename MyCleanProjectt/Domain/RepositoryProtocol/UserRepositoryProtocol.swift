ac07ea3d70b0cc0b1cf63bbc422102eed0c6b581
jhw <unity3013@dkitec.com>
2025-07-08 15:56:50 +0900

    UserListUsecase 구현, RepositoryProtocol 정의

---
+++ MyCleanProjectt/Domain/RepositoryProtocol/UserRepositoryProtocol.swift
@@ -0,0 +1,16 @@
+//
+//  UserRepositoryProtocol.swift
+//  MyCleanProjectt
+//
+//  Created by Hyunwoo Jeon on 7/8/25.
+//
+
+import Foundation
+
+public protocol UserRepositoryProtocol {
+    
+    func fetchUser(query: String, page: Int) async -> Result<UserListResult, NetworkError> //유저 리스트 불러오기 (원격)
+    func getFavoriteUsers() -> Result<[UserListItem], CoreDataError> //전체 즐겨찾기 리스트 불러오기
+    func saveFavoriteUser(user: UserListItem) -> Result<Bool, CoreDataError>
+    func deleteFavoriteUser(userID: String) -> Result<Bool, CoreDataError>
+}
