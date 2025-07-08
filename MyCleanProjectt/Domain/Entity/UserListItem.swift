810f1172ede1a8fde4a6df6084f7f4afca6dcf9e
jhw <unity3013@dkitec.com>
2025-07-08 15:10:25 +0900

    Domain
    - Entity 구현 (UserListItem, NetworkError, CoreDataError)

---
+++ MyCleanProjectt/Domain/Entity/UserListItem.swift
@@ -0,0 +1,46 @@
+//
+//  UserListItem.swift
+//  MyCleanProjectt
+//
+//  Created by Hyunwoo Jeon on 7/8/25.
+//
+
+import Foundation
+
+struct UserListResult: Decodable {
+    let totalCount: Int
+    let imcompleteResults: Bool
+    let items: [UserListItem]
+    
+    enum CodingKeys: String, CodingKey {
+        case totalCount = "total_count"
+        case imcompleteResults = "incomplete_results"
+        case items
+    }
+    
+    init(from decoder: any Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        self.totalCount = try container.decode(Int.self, forKey: .totalCount)
+        self.imcompleteResults = try container.decode(Bool.self, forKey: .imcompleteResults)
+        self.items = try container.decode([UserListItem].self, forKey: .items)
+    }
+}
+
+struct UserListItem: Decodable {
+    let id: Int
+    let login: String
+    let imageURL: String
+    
+    enum CodingKeys: CodingKey {
+        case id
+        case login
+        case imageURL
+    }
+    
+    init(from decoder: any Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        self.id = try container.decode(Int.self, forKey: .id)
+        self.login = try container.decode(String.self, forKey: .login)
+        self.imageURL = try container.decode(String.self, forKey: .imageURL)
+    }
+}
