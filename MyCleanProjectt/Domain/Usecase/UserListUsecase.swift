751291beb50d714484a679d5617fc1e29c1b8466
jhw <unity3013@dkitec.com>
2025-07-09 12:26:30 +0900

    Usecase 추가 구현

--- MyCleanProjectt/Domain/Usecase/UserListUsecase.swift
+++ MyCleanProjectt/Domain/Usecase/UserListUsecase.swift
@@ -14,15 +14,18 @@ public protocol UserListUsecaseProtocol {
     func saveFavoriteUser(user: UserListItem) -> Result<Bool, CoreDataError>
     func deleteFavoriteUser(userID: String) -> Result<Bool, CoreDataError>
     
-    //배열 -> Dictionary [초성: [유저리스트]]
     //유저리스트 - 즐겨찾기 포함된 유저인지
+    func checkFavoriteState(fetchUsers: [UserListItem], favoriteUsers: [UserListItem]) -> [(user: UserListItem, isFavorite: Bool)]
+    //배열 -> Dictionary [초성: [유저리스트]]
+    func convertListToDictionary(favoriteUsers: [UserListItem]) -> [String: [UserListItem]]
+    
 }
 
 public struct UserListUsecase: UserListUsecaseProtocol {
     
     private let repository: UserRepositoryProtocol
     
-    public init(repository: UserRepositoryProtocol) {
+    init(repository: UserRepositoryProtocol) {
         self.repository = repository
     }
     
@@ -40,6 +43,28 @@ public struct UserListUsecase: UserListUsecaseProtocol {
     
     public func deleteFavoriteUser(userID: String) -> Result<Bool, CoreDataError> {
         repository.deleteFavoriteUser(userID: userID)
+    }
+    
+    //유저리스트 - 즐겨찾기 포함된 유저인지
+    public func checkFavoriteState(fetchUsers: [UserListItem], favoriteUsers: [UserListItem]) -> [(user: UserListItem, isFavorite: Bool)] {
+        let favoriteSet = Set(favoriteUsers)
+        
+        return fetchUsers.map { user in
+            if favoriteSet.contains(user) {
+                return (user: user, isFavorite: true)
+            } else {
+                return (user: user, isFavorite: false)
+            }
+        }
+    }
+    //배열 -> Dictionary [초성: [유저리스트]]
+    public func convertListToDictionary(favoriteUsers: [UserListItem]) -> [String: [UserListItem]] {
+        return favoriteUsers.reduce(into: [String: [UserListItem]]()) { dict, user in
+            if let firstString = user.login.first {
+                let key = String(firstString).uppercased()
+                dict[key, default: []].append(user)
+            }
+        }
     }
     
     
