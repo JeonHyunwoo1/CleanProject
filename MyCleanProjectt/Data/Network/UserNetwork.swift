1219db327d8a88f03cdba214f8f16f3efd190109
jhw <unity3013@dkitec.com>
2025-07-09 10:53:01 +0900

    UserNetwork 구현

---
+++ MyCleanProjectt/Data/Network/UserNetwork.swift
@@ -0,0 +1,24 @@
+//
+//  UserNetwork.swift
+//  MyCleanProjectt
+//
+//  Created by Hyunwoo Jeon on 7/9/25.
+//
+
+import Foundation
+import Alamofire
+
+final public class UserNetwork {
+    private let networkManager: NetworkManagerProtocol
+    
+    init(networkManager: NetworkManagerProtocol) {
+        self.networkManager = networkManager
+    }
+    
+    func fetchUsers(query: String, page: Int) async -> Result<UserListResult, NetworkError> {
+        
+        let url = "https://api.github.com/search/users?q=\(query)&page=\(page)"
+        
+        return await networkManager.fetchData(url: url, method: .get, parameters: nil)
+    }
+}
